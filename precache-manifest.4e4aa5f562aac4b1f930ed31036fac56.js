792d449e7f781ae778b811a5ec60afb8c8a3b9de
Romario <romario.curcio@corp.globo.com>
2019-11-17 21:56:44 -0300

    Updates

---
+++ precache-manifest.4e4aa5f562aac4b1f930ed31036fac56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193fa20b2b2ee29dbfb165b265548589",
+    "url": "/teste-react/index.html"
+  },
+  {
+    "revision": "04579199cd81ba6cc47c",
+    "url": "/teste-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b6dffa41d5e476d21c85",
+    "url": "/teste-react/static/js/2.7b3b58a2.chunk.js"
+  },
+  {
+    "revision": "04579199cd81ba6cc47c",
+    "url": "/teste-react/static/js/main.a8574e25.chunk.js"
+  },
+  {
+    "revision": "ce5aa9c61ac1fd83f9cb",
+    "url": "/teste-react/static/js/runtime-main.2f594667.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/teste-react/static/media/logo.25bf045c.svg"
+  }
+]);
